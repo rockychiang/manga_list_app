daf452c52f5e2031c56fb0b8a0f9252e1abd5130
rockychiang <rocky@chem.ucla.edu>
2018-04-24 04:15:52 -0400

    created User create method to let people register new account. also render the register form again with red border if there are any errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,13 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+    else
+      render :new
+    end
   end
 
   def show
