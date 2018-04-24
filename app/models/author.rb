a8466dde773e0e23c9eb588f0eef0e20eec38a9d
rockychiang <rocky@chem.ucla.edu>
2018-04-24 00:23:18 -0400

    added validations to author and manga model

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,3 +1,4 @@
 class Author < ApplicationRecord
   has_many :mangas
+  validates :name, uniqueness: true, presence: true
 end
--- app/models/manga.rb
+++ app/models/manga.rb
@@ -2,4 +2,5 @@ class Manga < ApplicationRecord
   has_many :collections
   has_many :users, through: :collections
   belongs_to :author
+  validates :title, presence: true
 end
