1a419e2dcec250ef8e4da32ec0aceae5bdbe2871
rockychiang <rocky@chem.ucla.edu>
2018-04-24 03:43:19 -0400

    Added create in sessions to log in user with correct logins and reject one without

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,11 +1,16 @@
 class SessionController < ApplicationController
-  
-  def new
 
+  def new
   end
 
   def create
-
+    @user = User.find_by(name: params[:name])
+    if !!@user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+    else
+      render :new
+    end
   end
 
   def destroy
