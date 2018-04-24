35bbe1016fc738341e24290ea63958eab122faea
rockychiang <rocky@chem.ucla.edu>
2018-04-24 00:32:54 -0400

    Created Authors controller with index and show methods

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root 'static#home'
+
+  resources :authors, only: [:index, :show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
