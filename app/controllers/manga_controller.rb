6351d5e4a3f23519bf2c45779acb8ccc730be0c7
rockychiang <rocky@chem.ucla.edu>
2018-04-25 17:35:00 -0400

    Set require_login, require_admin permission for the appropriate manga routes and created a set_manga method for show, edit and update method

--- app/controllers/manga_controller.rb
+++ app/controllers/manga_controller.rb
@@ -1,4 +1,7 @@
 class MangaController < ApplicationController
+  before_action :require_login, :require_admin, except: [:index, :show]
+  before_action :set_manga, only: [:show, :edit, :update]
+
   def index
     @manga = Manga.all
   end
@@ -10,13 +13,18 @@ class MangaController < ApplicationController
   end
 
   def show
-    @manga = Manga.find(params[:id])
-    @collection = @manga.collections.find_or_initialize_by(user_id: current_user.id)
+    @collection = @manga.collections.find_or_initialize_by(user_id: current_user.id) if logged_in?
   end
 
   def edit
   end
 
   def update
+  end
+
+  private
+
+  def set_manga
+      @manga = Manga.find(params[:id])
   end
 end
