e8f191aaba577c3ab778eb682acae2c3bc450971
rockychiang <rocky@chem.ucla.edu>
2018-04-24 22:02:11 -0400

    Added years active method to manga model and added years active info to manga show page

--- app/models/manga.rb
+++ app/models/manga.rb
@@ -3,4 +3,10 @@ class Manga < ApplicationRecord
   has_many :users, through: :collections
   belongs_to :author
   validates :title, presence: true
+
+  def years_active
+    started = start_date.to_s.to_date.strftime("%b %e, %Y")
+    !!end_date ? ended = end_date.to_s.to_date.strftime("%b %e, %Y") : ended = "Present"
+    started + " - " + ended
+  end
 end
