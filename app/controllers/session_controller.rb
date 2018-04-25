0dba2b12ae7b24ff56a659b72acf6620dc4be739
rockychiang <rocky@chem.ucla.edu>
2018-04-25 04:57:35 -0400

    Changed login to require email instead of name

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -4,7 +4,7 @@ class SessionController < ApplicationController
   end
 
   def create
-    user = User.find_by(name: params[:name])
+    user = User.find_by(email: params[:email])
     if !!user && user.authenticate(params[:password])
       session[:user_id] = user.id
       redirect_to user_path(user)
