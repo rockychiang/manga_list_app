8d81f0a0e4054d75d4aa9c65e3b1aede3b8b3cb6
rockychiang <rocky@chem.ucla.edu>
2018-04-25 21:27:46 -0400

    Fixed manga form to correctly updates author and create new author if it couldn't find one. also fixed the dates in seed file

--- app/models/manga.rb
+++ app/models/manga.rb
@@ -5,6 +5,11 @@ class Manga < ApplicationRecord
   validates :title, presence: true
   accepts_nested_attributes_for :author
 
+  def author_attributes=(author_attributes)
+    a = Author.find_or_create_by(name: author_attributes[:name])
+    self.author = a
+  end
+
   def years_active
     started = start_date.to_s.to_date.strftime("%b %e, %Y")
     !!end_date ? ended = end_date.to_s.to_date.strftime("%b %e, %Y") : ended = "Present"
--- db/seeds.rb
+++ db/seeds.rb
@@ -21,27 +21,27 @@ end
 
 def make_authors_and_manga
   oda = Author.create(name: "Oda, Eiichiro")
-  oda.mangas.create(title: "One Piece", status: "On Going", start_date: 19970722)
+  oda.mangas.create(title: "One Piece", status: "On Going", start_date: "1997-07-22")
 
   watasuki = Author.create(name: "Watsuki, Nobuhiro")
-  watasuki.mangas.create(title: "Rurouni Kenshin", status: "Completed", volumes: 28, chapters: 259, start_date: 19940412, end_date: 19990921)
+  watasuki.mangas.create(title: "Rurouni Kenshin", status: "Completed", volumes: 28, chapters: 259, start_date: "1994-04-12", end_date: "1999-09-21")
 
   fujisawa = Author.create(name: "Fujisawa, Tohru")
-  fujisawa.mangas.create(title: "GTO", status: "Completed", volumes: 25, chapters: 208, start_date: 19961211, end_date: 20020130)
+  fujisawa.mangas.create(title: "GTO", status: "Completed", volumes: 25, chapters: 208, start_date: "1996-12-11", end_date: "2002-01-30")
 
   ishida = Author.create(name: "Ishida, Sui")
-  ishida.mangas.create(title: "Tokyo Ghoul:re", status: "On Going", start_date: 20141016)
-  ishida.mangas.create(title: "Tokyo Ghoul", status: "Completed", volumes: 14, chapters: 144, start_date: 20110908, end_date: 20140918)
+  ishida.mangas.create(title: "Tokyo Ghoul:re", status: "On Going", start_date: "2014-10-16")
+  ishida.mangas.create(title: "Tokyo Ghoul", status: "Completed", volumes: 14, chapters: 144, start_date: "2011-09-08", end_date: "2014-09-18")
 
   kishimoto = Author.create(name: "Kishimoto, Masashi")
-  kishimoto.mangas.create(title: "Naruto", status: "Completed", volumes: 72, chapters: 700, start_date: 19990921, end_date: 20141110)
+  kishimoto.mangas.create(title: "Naruto", status: "Completed", volumes: 72, chapters: 700, start_date: "1999-09-21", end_date: "2014-11-10")
 
   toriyama = Author.create(name: "Toriyama, Akira")
-  toriyama.mangas.create(title: "Dragon Ball", status: "Completed", volumes: 42, chapters: 520, start_date: 19841120, end_date: 19950523)
-  toriyama.mangas.create(title: "Dragon Ball Super", status: "On Going", start_date: 20150620)
+  toriyama.mangas.create(title: "Dragon Ball", status: "Completed", volumes: 42, chapters: 520, start_date: "1984-11-20", end_date: "1995-05-23")
+  toriyama.mangas.create(title: "Dragon Ball Super", status: "On Going", start_date: "2015-06-20")
 
   arakawa = Author.create(name: "Arakawa, Hiromu")
-  arakawa.mangas.create(title: "Fullmetal Alchemist", status: "Completed", volumes: 27, chapters: 116, start_date: 20010712, end_date: 20100911)
+  arakawa.mangas.create(title: "Fullmetal Alchemist", status: "Completed", volumes: 27, chapters: 116, start_date: "2001-07-12", end_date: "2010-09-11")
 end
 
 def make_collections
