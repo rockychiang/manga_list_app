58c816da23d9960b06f544c47bc06f5abd2c2aa9
rockychiang <rocky@chem.ucla.edu>
2018-05-15 22:15:18 -0400

    Edit next author link to change the page content

--- app/assets/javascripts/authors.js
+++ app/assets/javascripts/authors.js
@@ -3,9 +3,16 @@ $( () => {
     e.preventDefault();
     const nextId = parseInt($('#js-next-author').data('id')) + 1;
     $.get(`/authors/${nextId}`, (author) => {
+      $('#js-next-author').data("id", author.id);
       $('#authorName').text(author.name);
       $('#addManga').attr("href", `/authors/${author.id}/manga/new`);
-      
+      $('#mangaList').html('');
+      author.mangas.forEach(manga => {
+        const source = $("#manga-template").html();
+        const template = Handlebars.compile(source);
+        const html = template(manga);
+        $('#mangaList').append(html);
+      });
     }, 'json');
   });
 });
