beae2bce6383d4ef7671e1e3c82d3c1bfae64822
rockychiang <rocky@chem.ucla.edu>
2018-05-15 22:31:54 -0400

    Edit previous author link to change the content of the page and refactor page editing into its own function

--- app/assets/javascripts/authors.js
+++ app/assets/javascripts/authors.js
@@ -3,16 +3,29 @@ $( () => {
     e.preventDefault();
     const nextId = parseInt($('#js-next-author').data('id')) + 1;
     $.get(`/authors/${nextId}`, (author) => {
-      $('#js-next-author').data("id", author.id);
-      $('#authorName').text(author.name);
-      $('#addManga').attr("href", `/authors/${author.id}/manga/new`);
-      $('#mangaList').html('');
-      author.mangas.forEach(manga => {
-        const source = $("#manga-template").html();
-        const template = Handlebars.compile(source);
-        const html = template(manga);
-        $('#mangaList').append(html);
-      });
+      change(author);
+    }, 'json');
+  });
+
+  $('#js-previous-author').on('click', (e) => {
+    e.preventDefault();
+    const prevId = parseInt($('#js-previous-author').data('id')) - 1;
+    $.get(`/authors/${prevId}`, (author) => {
+      change(author);
     }, 'json');
   });
 });
+
+function change(author) {
+  $('#js-next-author').data("id", author["id"]);
+  $('#js-previous-author').data("id", author["id"]);
+  $('#authorName').text(author.name);
+  $('#addManga').attr("href", `/authors/${author["id"]}/manga/new`);
+  $('#mangaList').html('');
+  author.mangas.forEach(manga => {
+    const source = $("#manga-template").html();
+    const template = Handlebars.compile(source);
+    const html = template(manga);
+    $('#mangaList').append(html);
+  });
+}
