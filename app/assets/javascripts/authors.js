bce469c108c3fa6696bb21708894b49aa7db1640
rockychiang <rocky@chem.ucla.edu>
2018-05-15 22:03:12 -0400

    Edit author serializer to provide the correct information and hijack the next author button to change the author name and add manga link

--- app/assets/javascripts/authors.js
+++ app/assets/javascripts/authors.js
@@ -2,6 +2,10 @@ $( () => {
   $('#js-next-author').on('click', (e) => {
     e.preventDefault();
     const nextId = parseInt($('#js-next-author').data('id')) + 1;
-
+    $.get(`/authors/${nextId}`, (author) => {
+      $('#authorName').text(author.name);
+      $('#addManga').attr("href", `/authors/${author.id}/manga/new`);
+      
+    }, 'json');
   });
 });
--- app/serializers/manga_serializer.rb
+++ app/serializers/manga_serializer.rb
@@ -1,7 +1,3 @@
 class MangaSerializer < ActiveModel::Serializer
-  attributes :id, :title, :author, :status, :volumes, :chapters, :start_date, :end_date
-
-  def author
-    AuthorSerializer.new(object.author, root: false)
-  end
+  attributes :id, :title, :status, :volumes, :chapters, :start_date, :end_date
 end
