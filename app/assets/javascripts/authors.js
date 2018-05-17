e5f87362394b4f2ed95645b56551909534eed1ec
rockychiang <rocky@chem.ucla.edu>
2018-05-16 23:53:05 -0400

    Refactor author.js to be more DRY

--- app/assets/javascripts/authors.js
+++ app/assets/javascripts/authors.js
@@ -2,31 +2,31 @@ $(document).on('turbolinks:load', () => {
   $('#js-next-author').on('click', function(e) {
     e.preventDefault();
     const nextId = parseInt($(this).data('id')) + 1;
-    $.get(`/authors/${nextId}`, (author) => {
-      change(author);
-    }, 'json');
+    getAuthor(nextId);
   });
 
   $('#js-previous-author').on('click', function(e) {
     e.preventDefault();
     const prevId = parseInt($(this).data('id')) - 1;
-    $.get(`/authors/${prevId}`, (author) => {
-      change(author);
-    }, 'json');
+    getAuthor(prevId);
   });
-
-  function change(author) {
-    $('#js-next-author').data("id", author.id);
-    $('#js-previous-author').data("id", author.id);
-    $('#authorName').text(author.name);
-    $('#addManga').attr("href", `/authors/${author.id}/manga/new`);
-    $('#mangaList').html('');
-    author.mangas.forEach(manga => {
-      const source = $("#manga-template").html();
-      const template = Handlebars.compile(source);
-      const html = template(manga);
-      $('#mangaList').append(html);
-    });
-    $('.mangaAuthor').attr("href", `/authors/${author.id}`).text(author.name);
-  };
 });
+
+function getAuthor(id) {
+  $.get(`/authors/${id}`, (author) => { change(author) }, 'json');
+};
+
+function change(author) {
+  $('#js-next-author').data("id", author.id);
+  $('#js-previous-author').data("id", author.id);
+  $('#authorName').text(author.name);
+  $('#addManga').attr("href", `/authors/${author.id}/manga/new`);
+  $('#mangaList').html('');
+  author.mangas.forEach(manga => {
+    const source = $("#manga-template").html();
+    const template = Handlebars.compile(source);
+    const html = template(manga);
+    $('#mangaList').append(html);
+  });
+  $('.mangaAuthor').attr("href", `/authors/${author.id}`).text(author.name);
+};
